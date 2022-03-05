364bc0d1cba5d576958931633bcf53f674a020e3
Aldin Sabanovski <aldinsab@gmail.com>
2022-03-05 19:25:02 +0100

    Initial commit

---
+++ components/header.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="header">
+    <a href="/">Ethereal Staking Stats</a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
+<style lang="scss" scoped>
+.header {
+  font-weight: bold;
+  font-size: 44px;
+  // white-space: nowrap;
+}
+</style>
